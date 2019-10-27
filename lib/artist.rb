9069cc3d66a97168be963f495894c050228e363d
Jazmin M <jazminmerckel@gmail.com>
2019-10-27 23:11:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -54,13 +54,6 @@ class Artist
     end
   end 
   
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
   
 end 
 
