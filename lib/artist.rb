1c429c84d3d01467c3731c288630da8833b636f0
Jazmin M <jazminmerckel@gmail.com>
2019-10-27 22:27:38 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+class Artist 
+  
+  attr_accessor :name
+  
+  #class variable, where to put all of the artists
+  @@all = []
+  
+  # instance method 
+  def initialize(name)
+    @name = name 
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  # getter for class variable @@all
+  # other parts of code can access it
+  def self.all
+    @@all
+  end 
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def self.find_or_create_by_name(name)
+    if there is an artist with the name given 
+      return artist with name
+      
+    else 
+      create artist with that name
+      
+    end 
+  
+end 
+
+
+
+
---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song 
+  
+  attr_accessor :name, :artist
+  
+  #class variable, where to put all of the songs
+  @@all = []
+  
+  # instance method 
+  def initialize(name)
+    @name = name 
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  # getter for class variable @@all
+  # other parts of code can access it
+  def self.all
+    @@all
+  end 
+  
+  
+end
