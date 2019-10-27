1fccbdc4f9bd6d1fbeb798bb1fda48d04804bf1e
Jazmin M <jazminmerckel@gmail.com>
2019-10-27 22:49:28 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,10 @@
 class MP3Importer
   
-  attr_accessor :name
+  attr_accessor :test_music_path
   
   
-  def initialize(name)
-    @name = name 
+  def initialize(test_music_path)
+    @test_music_path = test_music_path 
   end
   
   
